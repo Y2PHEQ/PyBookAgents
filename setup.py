3f60f16c7e043182cafaa0ad0a416c4eef645fea
Sintacs Ao <151928881+sintxcs@users.noreply.github.com>
2024-02-14 17:04:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ LONG_DESCRIPTION = (THIS_DIR / "README.md").read_text()
 
 setup(
     name=PACKAGE,
-    version='0.0.1-DEV',
+    version='0.0.2',
     description="Random user agents generator for Facebook. It has 4 options: Dalvik, iPhone, Android, and Random (it's either Android or iPhone).",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
@@ -23,7 +23,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Education',
         'Operating System :: Microsoft :: Windows :: Windows 10',
-        'License :: OSI Approved :: MIT License',
+        'License :: MIT License',
         'Programming Language :: Python :: 3'
     ],
     #setup_requires=["wheel"],
